efb5186cbd979467e4b8ef4452911fb35d89ada4
ut1201 <ut1201@UNICOMTIC31>
2024-10-11 14:09:12 +0530

    Add project files.

---
+++ CourseManagementSystem_v1/Course.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class Course
+    {
+        private int CourseId { get; set; }
+        private string Title { get; set; }
+        private string Duration { get; set; }
+        private decimal Price { get; set; }
+
+        private Course(int courseId, string title, string duration, decimal price)
+        {
+            this.CourseId = courseId;
+            this.Title = title;
+            this.Duration = duration;
+            this.Price = price;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"ID: {CourseId}, Title: {Title}, Duration: {Duration}, Price: {Price}";
+        }
+    }
+}
